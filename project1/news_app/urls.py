f34c5338a66b78577b73a111c23bb915942cb24f
Bayram <bmbayrambek1600@gmail.com>
2024-07-22 14:57:08 +0500

    added some features of the website

--- project1/news_app/urls.py
+++ project1/news_app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import NewsView, news_detail, homeView
+from .views import NewsView, news_detail, homeView, contactView, ErrorView, aboutView
 
 urlpatterns = [
     path('', homeView, name='home'),
     path('all_news', NewsView, name='news'),
     path('news/<int:id>/', news_detail, name='news_detail'),
+    path('contact/', contactView, name='contact_page'),
+    path('error/', ErrorView, name='error'),
+    path('about/', aboutView, name='about_page'),
 ]
 
--- project1/news_app/views.py
+++ project1/news_app/views.py
@@ -1,6 +1,6 @@
+from urllib import request
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import detail
-
 from .models import NewsModel, Category
 
 
@@ -29,4 +29,18 @@ def homeView(request):
     }
     return render(request, template_name='news/home.html', context=context)
 
+
+def contactView(request):
+    context = {}
+    return render(request, template_name='news/contact.html', context=context)
+
+
+def ErrorView(request):
+    context = {}
+    return render(request, template_name='news/404.html', context=context)
+
+
+def aboutView(request):
+    context = {}
+    return render(request, template_name='news/about_page.html', context=context)
 
